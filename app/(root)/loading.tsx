33821a88db825844db81cd12709e86415eecf73a
ahmed zachadi <zachadi1979@gmail.com>
2024-02-11 21:27:28 +0100

    add loader

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,6 @@
+import PropagateLoader from "react-spinners/PropagateLoader";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <PropagateLoader color="#b2a22f" />;
+}
